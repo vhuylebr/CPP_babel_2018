92cf937b1c966b5e0c5929209a9e18fc59358e9d
Zakaria LAABID <zakaria.laabid@epitech.eu>
2018-09-21 10:48:47 +0200

    Done

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <boost/thread.hpp>
 #include "portaudio.h"
 #include "../include/PortAudio.hpp"
 #include "../include/VoIPManager.hpp"
 
+PaStream	*startRecord(PaStream *stream, paTestData &data) {
+	if ((stream = voIP.startRecordInput(data)) == NULL) {
+		std::cout << "An error occured" << std::endl;
+		exit(84);
+	}
+}
+
+PaStream	*playRecord(PaStream *stream, paTestData &data)
+{
+	if ((stream = voIP.playRecordOutput(data)) == NULL) {
+		std::cout << "An error occured" << std::endl;
+		exit(84);
+	}
+}
+
 int	main()
 {
 	Audio::VoIPManager  voIP;
+	Audio::PortAudio  portA;
 	PaStream*           stream;
 	paTestData          data;
-
-	while (1) {
-		if ((stream = voIP.startRecordInput(data)) == NULL) {
-			std::cout << "An error occured" << std::endl;
-			exit(84);
-		}
-		if ((stream = voIP.playRecordOutput(data)) == NULL) {
-			std::cout << "An error occured" << std::endl;
-			exit(84);
-		}
-	}
+	using namespace boost;
+    thread thread_1 = thread(startRecord, stream, data);
+    thread thread_2 = thread(playRecord, stream, data);
 }
