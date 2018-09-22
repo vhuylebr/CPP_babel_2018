28dd520af7597010c04fb36d77deb3cb788597a3
valerian <valerian.huylebroeck@epitech.eu>
2018-09-22 17:20:12 +0200

    add client UDP

---
+++ client/include/UdpClient.hpp
@@ -0,0 +1,34 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+#ifndef UDPCLIENT_HPP_
+	#define UDPCLIENT_HPP_
+class UdpClient
+{
+  public:
+    UdpClient(int port);
+    ~UdpClient();
+    void startCall(std::string&, int);
+    void hangUp();
+  private:
+    void start_sending();
+    void start_receive();
+    void handle_receive(const boost::system::error_code &error,
+                        std::size_t size);
+    void handle_send(boost::shared_ptr<std::string> message,
+                     const boost::system::error_code &error,
+                     std::size_t size);
+    udp::socket _socket;
+    int         _portHost;
+    udp::endpoint remote_endpoint_;
+    boost::array<char, 1024> recv_buffer_;
+};
+
+#endif /* UDPCLIENT_HPP_ */
---
+++ client/src/UdpClient.cpp
@@ -0,0 +1,80 @@
+/*
+** EPITECH PROJECT, 2018
+** babel
+** File description:
+** UdpClient
+*/
+
+#include "../include/UdpClient.hpp"
+
+std::string make_daytime_string()
+{
+    using namespace std; // For time_t, time and ctime;
+    time_t now = time(0);
+    return ctime(&now);
+}
+
+UdpClient::~UdpClient()
+{
+}
+
+UdpClient::UdpClient(int port)
+    : _portHost(port)
+{
+    std::cout << "init udpClient" << std::endl;
+}
+
+void UdpClient::hangUp() {
+    _socket.close();
+}
+
+void UdpClient::startCall(std::string &remoteIp, int remotePort) {
+    boost::asio::io_service ioService;
+    _socket = udp::socket(ioService);
+    // , udp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), _portHost)
+    remote_endpoint_ = udp::endpoint(boost::asio::ip::address::from_string(remoteIp.c_str()), remotePort);
+    start_receive();
+    start_sending();
+    ioService.run();
+}
+
+void UdpClient::start_sending()
+{
+    std::cout << "start sending" << std::endl;
+    boost::shared_ptr<std::string> message(
+        new std::string(make_daytime_string()));
+    _socket.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
+                          boost::bind(&UdpClient::handle_send, this, message,
+                                      boost::asio::placeholders::error,
+                                      boost::asio::placeholders::bytes_transferred));
+}
+void UdpClient::start_receive()
+{
+    std::cout << "start receive" << std::endl;
+    _socket.async_receive_from(
+        boost::asio::buffer(recv_buffer_), remote_endpoint_,
+        boost::bind(&UdpClient::handle_receive, this,
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+}
+
+void UdpClient::handle_receive(const boost::system::error_code &error,
+                               std::size_t size)
+{
+    if (!error || error == boost::asio::error::message_size)
+    {
+        std::cout << "Receive data " << std::string(recv_buffer_.data()) << " size: " << size << std::endl;
+    }
+    start_receive();
+}
+
+void UdpClient::handle_send(boost::shared_ptr<std::string> message,
+                            const boost::system::error_code &error,
+                            std::size_t size)
+{
+    if (!error || error == boost::asio::error::message_size)
+    {
+        std::cout << message->c_str() << std::endl;
+    }
+    start_sending();
+}
