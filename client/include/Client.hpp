94493fbcc8b076490784a67e3c65c2c6879d285b
Matthieu <matthieu.brault@epitech.eu>
2018-09-22 14:49:52 +0200

    client's hpp patch

--- client/include/Client.hpp
+++ client/include/Client.hpp
@@ -5,6 +5,7 @@
 ** Client
 */
 
+#include <unordered_map>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/bind.hpp>
@@ -20,15 +21,22 @@ class Client
   public:
     Client(boost::asio::io_service &io_service, std::string ip, int port);
     ~Client();
+  
   private:
     int _port;
     std::string _ip;
-    void start_receive();
     tcp::endpoint _endpoint;
     std::array<char, 1024> recv_buf;
     std::shared_ptr<tcp::socket> _socket;
-    void send_something(std::string message);
-    void handle_receive(const boost::system::error_code &error);
+    using ptrFunc = std::function<int(std::vector<std::string>)>;
+    std::unordered_map<std::string, ptrFunc>  _actions;
+
+    void		start_receive();
+    void		send_something(std::string message);
+    void		handle_receive(const boost::system::error_code &error);
+    static int	method1(std::vector<std::string>);
+	static int	method2(std::vector<std::string>);
+	void		execActions(const std::string &);
 };
 
 #endif /* !CLIENT_HPP_ */
