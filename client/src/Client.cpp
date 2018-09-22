bdc0bec89fb0b1f4bcaf6313accece4116e5a32b
Matthieu <matthieu.brault@epitech.eu>
2018-09-22 14:49:27 +0200

    client's table function

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -15,6 +15,8 @@ Client::Client(boost::asio::io_service &io_service, std::string ip, int port)
     : _endpoint(boost::asio::ip::address::from_string(ip.c_str()), port),
       _ip(ip), _port(port), _socket(std::make_shared<tcp::socket>(io_service))
 {
+    _actions["toto"] = Client::method1;
+    _actions["tata"] = Client::method2;
     _socket->connect(_endpoint);
     start_receive();
 }
@@ -28,11 +30,31 @@ void Client::start_receive()
 
 void Client::handle_receive(const boost::system::error_code &error)
 {
+    std::string cmd = std::string(recv_buf.data());
+
     std::cout << "handling receive: " << error << ": " << error.message() << std::endl;
-    std::cout << std::string(recv_buf.data()) << std::endl;
-    //fais le pinteur sur fonction ici stp
+    std::cout << cmd << std::endl;
+    execActions(cmd);
     send_something("send_somthing");
     start_receive();
+}
+
+void    Client::execActions(const std::string &cmd) {
+    std::size_t                 current;
+    std::size_t                 previous = 0;
+    std::vector<std::string>    info;
+
+    current = cmd.find(' ');
+    while (current != std::string::npos) {
+        info.push_back(cmd.substr(previous, current - previous));
+        previous = current + 1;
+        current = cmd.find(' ', previous);
+    }
+    info.push_back(cmd.substr(previous, current - previous));
+    if (!_actions[info[0]])
+        std::cerr << "This command doesn't exist" << std::endl;
+    else
+        _actions[info[0]](info);
 }
 
 void Client::send_something(std::string message)
@@ -43,3 +65,12 @@ void Client::send_something(std::string message)
     _socket->write_some(boost::asio::buffer(buf, message.size()), error);
 }
 
+int Client::method1(std::vector<std::string> cmd)
+{
+    return 0;
+}
+
+int Client::method2(std::vector<std::string> cmd)
+{
+    return 0;
+}
